c2bb8664b43e964116e41431f3fac84d6fc850a8
Eliza Weisman <eliza@buoyant.io>
2018-05-31 14:07:53 -0700

    Add `tls="true"` metric label to TLS connections

--- proxy/src/ctx/transport.rs
+++ proxy/src/ctx/transport.rs
@@ -37,6 +37,13 @@ impl Ctx {
             Ctx::Server(ref ctx) => &ctx.proxy,
         }
     }
+
+    pub fn is_tls(&self) -> bool {
+        match *self {
+            Ctx::Client(ref ctx) => ctx.is_tls,
+            Ctx::Server(ref ctx) => ctx.is_tls,
+        }
+    }
 }
 
 impl Server {
